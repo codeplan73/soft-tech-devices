103ac1245c944403348f58624d380662d35dd318
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-01-13 00:34:32 +0100

    completed dashboard card component

--- app/(auth)/components/DashboardCard.tsx
+++ app/(auth)/components/DashboardCard.tsx
@@ -7,10 +7,10 @@ interface Props {
 
 const DashboardCard = ({ title, amount, text }: Props) => {
   return (
-    <div className="flex items-center justify-between max-w-xs bg-white p-4">
+    <div className="flex items-center justify-between max-w-xs bg-white px-4 py-2 hover:shadow-xl drop-shadow-sm">
       <div className="flex flex-col items-start space-y-1">
         <h4 className="text-xs text-slate-500">{title}</h4>
-        <h4 className="text-2xl font-semibold">${amount}</h4>
+        <h4 className="text-xl font-semibold">${amount}</h4>
         <h4 className="text-xs text-slate-500">{text}</h4>
       </div>
       <div>map</div>
--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -7,11 +7,26 @@ const page = () => {
   return (
     <div className="w-full py-4 px-4 flex flex-col space-y-4">
       <Banner title="Dashboard" />
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <DashboardCard
-          title="Dashboard"
-          amount={20.43}
+          title="Total Stock"
+          amount={546}
+          text="We have 546 items in stock"
+        />
+        <DashboardCard
+          title="Total Sales"
+          amount={435}
           text="we have sold 123 items"
+        />
+        <DashboardCard
+          title="Total Orders"
+          amount={53}
+          text="we have 53 Orders"
+        />
+        <DashboardCard
+          title="Total Swap"
+          amount={19}
+          text="we have 19 Swap deals"
         />
       </div>
       <div className="bg-white px-4 py-4 rounded-md shadow-xl">
