dfc4bf3f0d2b950608fcb5b2009ffcfa5fbcda05
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-01-15 22:54:30 +0100

    get single product

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,4 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { productSchema } from '@/app/schema'
+import prisma from '@/prisma/client'
+
 // get single product
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const productDetails = await prisma.product.findUnique({
+      where: { id: params.id },
+    })
+    return NextResponse.json(productDetails, { status: 200 })
+  } catch (error) {
+    return NextResponse.json({ error: 'Invalid Product Id' }, { status: 404 })
+  }
+}
 
 // update single product
 
